45a4f09aca4e28ab574cebfbe1ba040bf5bd0969
rc1208 <chowdhuryrahul5@gmail.com>
2019-09-15 15:54:27 -0600

    converted code into more object oriented, coz eh why not?

--- code/transform_files.py
+++ code/transform_files.py
@@ -1,54 +1,58 @@
 __author__ = "Rahul Chowdhury"
+
 import os
 import shutil
-def main():
-    src_images = "../data/Corresponding Images/"
-    src_annotated = "../data/Annotated  2/"
-    images_dir = "../data/images/"
-    tagged_dir = "../data/tagged/"
+
+class TransformFiles:
+    def __init__(self, src_images, src_annotated, images_dir, tagged_dir):
+        self.src_images = src_images
+        self.src_annotated = src_annotated
+        self.images_dir = images_dir
+        self.tagged_dir = tagged_dir
 
     #clean images folder and tagged folder if either already exists
-    if os.path.exists(images_dir):
-        shutil.rmtree(images_dir)
-    if os.path.exists(tagged_dir):
-        shutil.rmtree(tagged_dir)
-    # else create images dir with all the data from downloaded image folder
-    os.mkdir(images_dir)
-    os.mkdir(tagged_dir)
+    def cleanFolders(self):
+       if os.path.exists(self.images_dir):
+           shutil.rmtree(self.images_dir)
+       if os.path.exists(self.tagged_dir):
+           shutil.rmtree(self.tagged_dir)
+
+    # create images dir with all the data from downloaded image folder
+    def createFolders(self):
+       os.mkdir(self.images_dir)
+       os.mkdir(self.tagged_dir)
 
     #copy the annotated files from annotated folder into tagged/ only if there is a corresponding_image_file
-    src_annotated_files = os.listdir(src_annotated)
-    src_images_files = os.listdir(src_images)
+    def copyAndRenameFiles(self):
+       src_annotated_files = os.listdir(self.src_annotated)
+       src_images_files = os.listdir(self.src_images)
+
+       i = 1
+       for file in src_annotated_files:
+           #print(file)
+
+           full_file_name = os.path.join(self.src_annotated,file)
+           corresponding_image_name_substring = file[:file.find('.xml')]
+
+           #matching[0] will contain the corrresponding image file name
+           matching = list(filter(lambda x: corresponding_image_name_substring in x, src_images_files))
 
 
-    i = 1
-    for file in src_annotated_files:
-        #print(file)
+           #copy xml file to tagged/
+           shutil.copyfile(self.src_annotated+file,self.tagged_dir+file)
+           #rename the file
+           os.rename(self.tagged_dir+file,self.tagged_dir + str(i) + ".xml")
 
-        full_file_name = os.path.join(src_annotated,file)
-        corresponding_image_name_substring = file[:file.find('.xml')]
-
-        #matching[0] will contain the corrresponding image file name
-        matching = list(filter(lambda x: corresponding_image_name_substring in x, src_images_files))
-
-
-        #copy xml file to the tagged/
-        shutil.copyfile(src_annotated+file,tagged_dir+file)
-        #rename the file
-        os.rename(tagged_dir+file,tagged_dir + str(i) + ".xml")
-
-        #copy image files to images/
-        if len(matching) != 0: #there is a matching image file
-            shutil.copyfile(src_images+matching[0],images_dir+matching[0])
-            #rename the file
-            os.rename(images_dir+matching[0],images_dir + str(i) + ".jpg")
-            i+=1
+           #copy image files to images/
+           if len(matching) != 0: #there is a matching image file
+               shutil.copyfile(self.src_images+matching[0],self.images_dir+matching[0])
+               #rename the file
+               os.rename(self.images_dir+matching[0],self.images_dir + str(i) + ".jpg")
+               i+=1
 
 
-
-
-
-
-#driver code
-if __name__ == "__main__":
-    main()
+# place variables here -> (downloaded image folder, downloaded annotated folder, dumping image folder, dumping annotated folder)
+obj = TransformFiles("../data/Corresponding Images/", "../data/Annotated  2/", "../data/images/", "../data/tagged/")
+obj.cleanFolders()
+obj.createFolders()
+obj.copyAndRenameFiles()
